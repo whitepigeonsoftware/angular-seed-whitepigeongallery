c1833f2caa371974f47615f562d5ae67ad36aeb9
Lukasz Rzeszotarski <lukasz.rzeszotarski@bearingpoint.com>
2018-02-04 22:34:38 +0100

    instagram connection

--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import {InstagramService} from '../shared/instagram/instagram.service';
+import { InstagramService } from '../shared/instagram/instagram.service';
 
 @NgModule({
   imports: [HomeRoutingModule, SharedModule],
